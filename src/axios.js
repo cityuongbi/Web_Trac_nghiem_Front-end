0cedb2810882014e8888babb047a676efb6dd194
cityuongbi <quannm.ptit@gmail.com>
2023-07-13 02:39:22 +0700

    Editing axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = "https://demo_project-1-d5070894.deta.app/";
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
